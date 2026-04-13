047faa53af604f338fa7c0d6813b9c4f2099e228
Reza Panahi <rezabumanpanahi@gmail.com>
2026-04-13 10:23:37 +0430

    simple Projects structure changed

---
+++ Simple-DB-Projects/Library Management/Readme.md
@@ -0,0 +1,11 @@
+## Scenario
+### Library Management Database System
+The library management system efficiently manages the library operations. In this database system, we store information about books, book names and numbers, details of the book borrower and library staff details, and so on. 
+
+The main objective of this database design project idea is help to 
+- Keep a record of the books available in the library 
+- Automate the process of book lending and returns
+- Assisting easily in book searches and availability checks
+- It also helps to keep a record of the staff details and the fines paid by late returners 
+
+## Logical Design
--- Simple-DB-Projects/Library Management/sql/Schema.sql
+++ Simple-DB-Projects/Library Management/sql/Schema.sql
@@ -1,19 +1,3 @@
-/*
-Library Management Database System
-The library management system efficiently manages the library operations. In this database system, we store information about books, book names and numbers, details of the book borrower and library staff details, and so on. 
-
-The main objective of this database design project idea is help to 
-- Keep a record of the books available in the library 
-- Automate the process of book lending and returns
-- Assisting easily in book searches and availability checks
-- It also helps to keep a record of the staff details and the fines paid by late returners 
-
-In the library database management system project, you can create different entities and their attribute as follows 
-- Book: ISBN, Title, Author name, Publication year, Publisher, Edition, genre, Price, Availability.
-- Member: Member ID, Name, Membership Type, Address, Phone Number, Email, Membership Status.
-- Transaction: Transaction ID, Member ID, ISBN, Issue Date, Return Date, Fine.
-- Staff: Staff ID, Name, Login time, log-out time, Role, Working Days, Working Hours.
-*/
 create database Library_Management;
 use Library_Management;
 
